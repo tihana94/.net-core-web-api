d99158da1ebebd984746750e61c988bf2ef4ffba
tihanak <tihanakrivic94@gmail.com>
2023-01-10 14:59:35 +0100

    Here is used api, entity framwork, dbContextClass, migration

---
+++ APIfornetapplication/APIfornetapplication/Data/dbcontext.cs
@@ -0,0 +1,16 @@
+﻿using APIfornetapplication.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIfornetapplication.Data
+{
+    public class dbcontext:DbContext
+    {
+        public dbcontext(DbContextOptions<dbcontext> options) : base(options)
+        {
+
+        }
+        public DbSet<Region>Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
--- APIfornetapplication/APIfornetapplication/Program.cs
+++ APIfornetapplication/APIfornetapplication/Program.cs
@@ -1,3 +1,6 @@
+using APIfornetapplication.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//for database injection
+builder.Services.AddDbContext<dbcontext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Walksconnections"));
+});
 
 var app = builder.Build();
 
